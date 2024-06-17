35fc8f040df92c415dbed2ec34017af42d5c22f2
Lith <LithWang@outlook.com>
2024-06-17 19:41:57 +0800

    SqlExecutor

--- src/Vit.Db/Vit.Db/Util/Data/Extensions/Execute/IDbConnection_Execute_Extensions.cs
+++ src/Vit.Db/Vit.Db/Util/Data/Extensions/Execute/IDbConnection_Execute_Extensions.cs
@@ -12,27 +12,7 @@ namespace Vit.Extensions.Linq_Extensions.Execute
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int Execute(this IDbConnection conn, string sql, IDictionary<string, object> param = null, IDbTransaction transaction = null, int? commandTimeout = null)
         {
-            // #1 setup command
-            using var cmd = conn.CreateCommand();
-            if (transaction != null) cmd.Transaction = transaction;
-            if (commandTimeout.HasValue) cmd.CommandTimeout = commandTimeout.Value;
-            cmd.Connection = conn;
-            cmd.CommandText = sql;
-            AddParameter(cmd, param);
-
-
-            // #2 execute
-            bool wasClosed = conn.State == ConnectionState.Closed;
-            try
-            {
-                if (wasClosed) conn.Open();
-                return cmd.ExecuteNonQuery();
-            }
-            finally
-            {
-                if (wasClosed) conn.Close();
-            }
-
+            return SqlExecutor.Instance.Execute(conn: conn, sql: sql, param: param, transaction: transaction, commandTimeout: commandTimeout);
         }
 
 
@@ -40,25 +20,7 @@ namespace Vit.Extensions.Linq_Extensions.Execute
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static object ExecuteScalar(this IDbConnection conn, string sql, IDictionary<string, object> param = null, IDbTransaction transaction = null, int? commandTimeout = null)
         {
-            // #1 setup command
-            using var cmd = conn.CreateCommand();
-            if (transaction != null) cmd.Transaction = transaction;
-            if (commandTimeout.HasValue) cmd.CommandTimeout = commandTimeout.Value;
-            cmd.Connection = conn;
-            cmd.CommandText = sql;
-            AddParameter(cmd, param);
-
-            // #2 execute
-            bool wasClosed = conn.State == ConnectionState.Closed;
-            try
-            {
-                if (wasClosed) conn.Open();
-                return cmd.ExecuteScalar();
-            }
-            finally
-            {
-                if (wasClosed) conn.Close();
-            }
+            return SqlExecutor.Instance.ExecuteScalar(conn: conn, sql: sql, param: param, transaction: transaction, commandTimeout: commandTimeout);
         }
 
 
@@ -66,56 +28,9 @@ namespace Vit.Extensions.Linq_Extensions.Execute
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IDataReader ExecuteReader(this IDbConnection conn, string sql, IDictionary<string, object> param = null, IDbTransaction transaction = null, int? commandTimeout = null)
         {
-
-            IDbCommand cmd = null;
-
-            bool wasClosed = conn.State == ConnectionState.Closed, disposeCommand = true;
-            try
-            {
-                // #1 setup command
-                cmd = conn.CreateCommand();
-                if (transaction != null) cmd.Transaction = transaction;
-                if (commandTimeout.HasValue) cmd.CommandTimeout = commandTimeout.Value;
-                cmd.Connection = conn;
-                cmd.CommandText = sql;
-                AddParameter(cmd, param);
-
-                // #2 execute
-                var commandBehavior = wasClosed ? CommandBehavior.CloseConnection : CommandBehavior.Default;
-                if (wasClosed) conn.Open();
-
-                var reader = cmd.ExecuteReader(commandBehavior);
-                wasClosed = false; // don't dispose before giving it to them!
-                disposeCommand = false;
-                return reader;
-            }
-            finally
-            {
-                if (wasClosed) conn.Close();
-                if (disposeCommand)
-                {
-                    //cmd.Parameters.Clear();
-                    cmd.Dispose();
-                }
-            }
-
+            return SqlExecutor.Instance.ExecuteReader(conn: conn, sql: sql, param: param, transaction: transaction, commandTimeout: commandTimeout);
         }
 
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static void AddParameter(IDbCommand cmd, IDictionary<string, object> param)
-        {
-            if (param != null)
-            {
-                foreach (var entry in param)
-                {
-                    var p = cmd.CreateParameter();
-                    p.ParameterName = entry.Key;
-                    p.Value = entry.Value ?? DBNull.Value;
-                    cmd.Parameters.Add(p);
-                }
-            }
-        }
 
     }
 }
